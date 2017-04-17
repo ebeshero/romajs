e4c7a47dcea35c7ec9e2397c1accebacf71d013c
raffazizzi <raffaeleviglianti@gmail.com>
2017-04-17 10:23:31 -0400

    Updated to Webpack 2 and React 15.4

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,9 @@
 import { jsdom } from 'jsdom'
-import xmldom from 'xmldom'
 import * as FormData from 'form-data'
 import fs from 'fs'
 
 global.document = jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
-global.window.DOMParser = xmldom.DOMParser
 global.FormData = FormData.default
 global.Blob = function (strings, encoding) {return Buffer.from(strings[0], encoding)}
 global.navigator = global.window.navigator
